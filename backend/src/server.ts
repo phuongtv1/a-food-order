23138509eaf96fc0de0124283560fdbaf34f118e
phuongtv1 <adayvn@gmail.com>
2022-07-29 14:01:00 +0700

    Deploying To Heroku

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import { dbConnect } from './configs/database.config';
 import express from "express";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import path from "path";
 import foodRouter from './routers/food.router';
 import userRouter from './routers/user.router';
 import orderRouter from './routers/order.router';
@@ -25,8 +26,11 @@ app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-
-const port = 5000;
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'public', 'index.html'))
+})
+const port =process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(port);
 });
